0232a9c49f1f28e86248f0ea85598dda8e6ce8e9
Fabian Benitez <fabianbenitez_87@hotmail.com>
2022-07-27 11:59:00 -0300

    practices proyects

---
+++ testimonios/src/componentes/Testimonio.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css'
+
+function Testimonio(props){
+  return (
+    <div className='contenedor-testimonio'>
+      <img 
+        className='imagen-testimonio'
+        src={require(`../imagenes/${props.foto}.png`)} 
+        alt= {`Foto de ${props.foto}`} />
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> in {props.pais}
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} at <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'> 
+          "{props.testimonio}" 
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
