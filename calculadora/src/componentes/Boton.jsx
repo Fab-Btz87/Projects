d83f65c9b30a2f13ad02879bc86b291b9f7ab24d
Fabian Benitez <fabianbenitez_87@hotmail.com>
2022-08-10 14:34:28 -0300

    Calculadora React

--- calculadora/src/componentes/Boton.jsx
+++ calculadora/src/componentes/Boton.jsx
@@ -3,14 +3,42 @@ import '../hojas-de-estilo/Boton.css';
 
 function Boton(props) {
   const esOperador = valor => {
-    return isNaN(valor) && (valor != '.' && (valor != '='));
+    return isNaN(valor) && (valor !== '.' && (valor !== '='));
   }
+
   return (
     <div
-      className={`boton-container ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+      className={`boton-container ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+      onClick={() => props.manejarClic(props.children)}>
       {props.children}
     </div>
-  )
+  );
+  //otra opcion es hacer una validacion sensilla
+  // if (esOperador(props.children)){
+  //   return (
+  //     <div
+  //       className='boton-container operador'
+  //       onClick={() => props.manejarClic(props.children)}>
+  //       {props.children}
+  //     </div>
+  //   );
+  // }else {
+  //   return (
+  //     <div
+  //       className='boton-container'
+  //       onClick={() => props.manejarClic(props.children)}>
+  //       {props.children}
+  //     </div>
+  //   )
+  // }
+  //o usar la equiq ueta button en vez de un div
+  // return (
+  //   <button
+  //     className={`boton-container ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+  //     onClick={() => props.manejarClic(props.children)}>
+  //     {props.children}
+  //   </button>
+  // );
 }
 
 export default Boton;
